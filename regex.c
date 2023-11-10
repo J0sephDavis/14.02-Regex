0b18ce215495d1cd3baf461d9fc6a302cab441d3
Joseph Davis <josephdavisjoey@gmail.com>
2023-11-10 14:45:39 -0500

    added main function to get the expr & text from args & added longest start match function

--- regex.c
+++ regex.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* --RULES--
+ * c matches any literal character c
+ * . matches any single character
+ * ^ matches beginning of the input string
+ * $ matches the end of the input string
+ * * matches zero or more occurrences of the previous character
+ */
 /*** prototypes ***/
 int matchstar(int c, char* regexp, char *text);
 int match(char* regexp, char *text);
@@ -51,11 +61,58 @@ int matchhere(char *regexp, char *text) {
 }
 
 //search for c*regexp at beginning of text
-//c -> the character we wish to see more of
+//this is the shortest left-most wildcard match
 int matchstar(int c, char *regexp, char *text) {
 	do { //a * matches zero or more instances
  		if (matchhere(regexp,text))
 			return 1;
 	} while(*text != '\0' && (*text++ == c || c == '.'));
+	return 0;
+}
+//the longest left-most wildcard match
+int long_matchstar(int c, char *regexp, char *text) {
+	char *t;
+	for (t = text; *t != '\0' && (*t == c || c == '.'); t++);
+
+	do { //matches zero or more instances
+ 		if (matchhere(regexp,t))
+			return 1;
+	} while(t-- > text);
+	return 0;
+}
+
+int main(int argc, char* argv[]) {
+	if (argc < 3) return -1;
+	//arg 0 is process name
+	//arg 1 is the regexpr
+	//arg 2 & beyond = input_text
+	char* regexpr = NULL;
+	char* input_text = NULL;
+	//get regexpr
+	regexpr = malloc(strlen(argv[2]));
+	if (regexpr == NULL) return -1;
+	strncpy(regexpr, argv[1], strlen(argv[2]));
+	//get input_text
+	int len = 0;
+	int a;
+
+	for (a = 2; a < argc; a++)
+		len += strlen(argv[a]);
+	len += (argc-2);
+	input_text = malloc(len);
+	if (input_text == NULL) return -1;
+	//
+	char* insertion_point = input_text;
+	for (a = 2; a < argc; a++) {
+		int arg_len = strlen(argv[a]);
+		strncpy(insertion_point, argv[a], arg_len);
+		insertion_point += arg_len;
+		if ((a+1) < argc)
+			*insertion_point++ = ' ';
+	}
+	printf("expr[%s]\ntext[%s]\n", regexpr,input_text);
+	//
+	free(input_text);
+	free(regexpr);
 	return 0;
 }
